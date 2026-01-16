d4841df0e920077cd81f6339c70fefcf4d7db6a8
andreas <andreassklizovic@gmail.com>
2026-01-16 16:49:05 +0100

    changes
    client changes

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+// Optional: Add a GET /logiranje route for browser access
+app.get('/logiranje', (req, res) => {
+  res.json({ message: 'This endpoint is for POST login only. Use POST /api/users/logiranje for API login.' });
+});
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from './db/connectDB.js';

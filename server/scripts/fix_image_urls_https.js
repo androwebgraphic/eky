4f0413181a73f290ba63d7fbf08d9b62ba962f98
andreas <andreassklizovic@gmail.com>
2026-01-21 01:43:22 +0100

    Create fix_image_urls_https.js

---
+++ server/scripts/fix_image_urls_https.js
@@ -0,0 +1,49 @@
+// Script to update all Dog image URLs in MongoDB from http to https
+// Usage: node scripts/fix_image_urls_https.js
+
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/eky';
+
+const dogSchema = new mongoose.Schema({}, { strict: false, collection: 'dogs' });
+const Dog = mongoose.model('Dog', dogSchema);
+
+async function fixDogImageUrls() {
+  await mongoose.connect(mongoUri);
+  const dogs = await Dog.find({
+    $or: [
+      { 'images.url': { $regex: '^http://' } },
+      { 'thumbnail.url': { $regex: '^http://' } }
+    ]
+  });
+  let updated = 0;
+  for (const dog of dogs) {
+    let changed = false;
+    if (Array.isArray(dog.images)) {
+      dog.images = dog.images.map(img => {
+        if (img.url && img.url.startsWith('http://')) {
+          changed = true;
+          return { ...img, url: img.url.replace('http://', 'https://') };
+        }
+        return img;
+      });
+    }
+    if (dog.thumbnail && dog.thumbnail.url && dog.thumbnail.url.startsWith('http://')) {
+      dog.thumbnail.url = dog.thumbnail.url.replace('http://', 'https://');
+      changed = true;
+    }
+    if (changed) {
+      await dog.save();
+      updated++;
+    }
+  }
+  console.log(`Updated ${updated} dog documents.`);
+  await mongoose.disconnect();
+}
+
+fixDogImageUrls().catch(err => {
+  console.error(err);
+  process.exit(1);
+});

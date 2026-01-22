71ee3efab8e7bae53c3536670e82321b7ceec452
andreas <andreassklizovic@gmail.com>
2026-01-22 20:03:40 +0100

    Update CardSmall.tsx

--- client/src/components/CardSmall.tsx
+++ client/src/components/CardSmall.tsx
@@ -311,7 +311,7 @@ const CardSmall: React.FC<CardSmallProps> = (props) => {
             cursor: images && images.length > 0 ? 'pointer' : 'default',
             width: '100%',
             height: 'auto',
-            aspectRatio: '1/1',
+            // aspectRatio: '1/1',
             overflow: 'hidden',
             display: 'flex',
             alignItems: 'center',
